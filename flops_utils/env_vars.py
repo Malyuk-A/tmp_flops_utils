694255edb2bb85c5441079c72d5a1e8d5e64dca4
Malyuk-A <malyuk.alexander1999@gmail.com>
2024-05-25 12:03:08 +0200

    Improve env vars

--- flops_utils/env_vars.py
+++ flops_utils/env_vars.py
@@ -4,7 +4,9 @@ from typing import Union
 
 from flops_utils.logging import logger
 
-ERROR_MESSAGE = (
+DOCKER_HOST_IP_LINUX = "172.17.0.1"
+
+_ERROR_MESSAGE = (
     "Terminating. Make sure to set the environment variables first. Missing: "
 )
 
@@ -12,6 +14,6 @@ ERROR_MESSAGE = (
 def get_env_var(name: str, default: Union[str, int] = None) -> str:
     env_var = os.environ.get(name) or default
     if env_var is None:
-        logger.fatal(f"{ERROR_MESSAGE}'{name}'")
+        logger.fatal(f"{_ERROR_MESSAGE}'{name}'")
         sys.exit(1)
     return env_var
